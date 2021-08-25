cf5cd901583defb5ac979841565835b564104059
Klaus Kuehnhammer <klaus@bitstem.com>
2021-08-25 18:01:51 +0200

    switched to 5GMAG license

--- include/IpSec.h
+++ include/IpSec.h
@@ -2,18 +2,16 @@
 //
 // Copyright (C) 2021 Klaus Kühnhammer (Österreichische Rundfunksender GmbH & Co KG)
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// Licensed under the License terms and conditions for use, reproduction, and
+// distribution of 5G-MAG software (the “License”).  You may not use this file
+// except in compliance with the License.  You may obtain a copy of the License at
+// https://www.5g-mag.com/reference-tools.  Unless required by applicable law or
+// agreed to in writing, software distributed under the License is distributed on
+// an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.
+// 
+// See the License for the specific language governing permissions and limitations
+// under the License.
 //
 #pragma once
 #include <string>
