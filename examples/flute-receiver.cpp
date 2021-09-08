98596af6e6073d19eed57a420586c492312aa144
Klaus Kuehnhammer <klaus@bitstem.com>
2021-09-08 09:34:54 +0200

    Switch to 5G-Mag license

--- examples/flute-receiver.cpp
+++ examples/flute-receiver.cpp
@@ -1,4 +1,18 @@
-
+// libflute - FLUTE/ALC library
+//
+// Copyright (C) 2021 Klaus Kühnhammer (Österreichische Rundfunksender GmbH & Co KG)
+//
+// Licensed under the License terms and conditions for use, reproduction, and
+// distribution of 5G-MAG software (the “License”).  You may not use this file
+// except in compliance with the License.  You may obtain a copy of the License at
+// https://www.5g-mag.com/reference-tools.  Unless required by applicable law or
+// agreed to in writing, software distributed under the License is distributed on
+// an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.
+// 
+// See the License for the specific language governing permissions and limitations
+// under the License.
+//
 #include <cstdio>
 #include <iostream>
 #include <argp.h>
