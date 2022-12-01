09030427d0a728269452912651936ef30691fa3c
Nicolas Josef Zunker <n.zunker@campus.tu-berlin.de>
2022-12-01 16:21:52 +0100

    cmakelists integration of freeRaptor

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-\cp freeRaptor/*.c src/
-\cp freeRaptor/*.h include/
-ls src/*.c | xargs -I{} mv {} {}pp
 rm -rf build
 mkdir build && cd build
 cmake -GNinja ..
 ninja
-cd ../src
-ls ../freeRaptor/*.c | awk -F "/" '{print $3"pp"}' | xargs rm -rf
-cd ../include
-ls ../freeRaptor/*.h | awk -F "/" '{print $3}' | xargs rm -rf
