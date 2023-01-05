04f24e2d465ef6d69deb2c9e4e5078fe73f680ff
SteezyE <eliaspichottka@gmail.com>
2023-01-05 09:57:41 +0100

    remove old freeRaptor header

--- include/RaptorFEC.h
+++ include/RaptorFEC.h
@@ -1,8 +1,6 @@
 #ifndef LIBFLUTE_RAPTORFEC_H
 #define LIBFLUTE_RAPTORFEC_H
 
-#include "gf2matrix.h" // freeRaptor lib
-#include "raptor10.h"  // freeRaptor lib
 #include "raptor.h"    // raptor lib
 #include "flute_types.h"
 #include "spdlog/spdlog.h"
