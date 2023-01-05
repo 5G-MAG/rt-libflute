605f14f871bdba2d057c5d48658be206825b74b9
SteezyE <eliaspichottka@gmail.com>
2023-01-05 09:43:30 +0100

    resolve merge conflict

--- include/RaptorFEC.h
+++ include/RaptorFEC.h
@@ -1,6 +1,10 @@
 #ifndef LIBFLUTE_RAPTORFEC_H
 #define LIBFLUTE_RAPTORFEC_H
 
-#include "raptor.h"
+#include "gf2matrix.h" // freeRaptor lib
+#include "raptor10.h"  // freeRaptor lib
+#include "raptor.h"    // raptor lib
+#include "flute_types.h"
+#include "spdlog/spdlog.h"
 
 #endif //LIBFLUTE_RAPTORFEC_H
